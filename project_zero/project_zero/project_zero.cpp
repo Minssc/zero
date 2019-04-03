4cc2529a2d37276b0ac445bca4bffd7ddfaf9a4f
Steven Kim <pauaup@naver.com>
2019-04-04 01:24:01 +0900

    progress sync

--- project_zero/project_zero/project_zero.cpp
+++ project_zero/project_zero/project_zero.cpp
@@ -133,28 +133,66 @@ void processImage(Mat image) {
 	//imshow("BLUR", image_gray);
 
 	//threshold(image_gray, image_gray, 50, 255, THRESH_BINARY); 
-	adaptiveThreshold(image_gray, image_gray, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 47, 2);
-	imshow("THRESHOLD", image_gray);
+	//Mat gaussian;
+	//Mat mean;
 
-	medianBlur(image_gray, image_gray, 3); // remove noise 
-	imshow("MEDIAN BLUR", image_gray);
-	medianBlur(image_gray, image_gray, 3); // remove noise 
-	medianBlur(image_gray, image_gray, 3); // remove noise 
-	medianBlur(image_gray, image_gray, 3); // remove noise 
-	medianBlur(image_gray, image_gray, 3); // remove noise 
-	medianBlur(image_gray, image_gray, 3); // remove noise 
-	medianBlur(image_gray, image_gray, 3); // remove noise 
-	imshow("MEDIAN BLUR2", image_gray);
+	//adaptiveThreshold(image_gray, mean, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 11, 2);
+	adaptiveThreshold(image_gray, image_gray, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 2);
+	//imshow("GAUSSIAN", gaussian);
+	//imshow("mean", mean);
 
-	Mat dilElement = getStructuringElement(MORPH_RECT, { 3,3 });
+	for (int i = 0; i < 10; i++){
+		medianBlur(image_gray, image_gray, 3); // remove noise 
+		//medianBlur(gaussian, gaussian, 3); // remove noise 
+		//medianBlur(mean, mean, 3); // remove noise 
+	}
+	//imshow("MEDIAN BLUR", image_gray);
 
+	/*
+	Mat dilElement = getStructuringElement(MORPH_DILATE, { 3,3 });
+	Mat erodElement = getStructuringElement(MORPH_ERODE, { 3,3 });
+	
 	dilate(image_gray, image_gray, dilElement);
-	imshow("DILATE", image_gray);
-	erode(image_gray, image_gray, dilElement);
-	imshow("ERODE", image_gray);
+	dilate(image_gray, image_gray, dilElement);
+	dilate(image_gray, image_gray, dilElement);
+	erode(image_gray, image_gray, erodElement);
+	erode(image_gray, image_gray, erodElement);
+	erode(image_gray, image_gray, erodElement);
+	*/
+	imshow("END PRODUCT", image_gray);
+	//imshow("END PRODUCT-g", gaussian);
+	//imshow("END PRODUCT-m", mean);
 	//Canny(image_gray, image_gray, 150, 200); 
 	//imshow("CANNY", image_gray); 
 	
+	Mat image_masked = image.clone(); 
+	for (int r = 0; r < image.rows; r++) { // apply mask to image 
+		for (int c = 0; c < image.cols; c++) {
+			Vec3b P = image_masked.at<Vec3b>(r, c);
+
+			if (image_gray.at<uchar>(r, c) == 255)
+				continue; 
+			P[0] = 0; P[1] = 0; P[2] = 0; 
+			image_masked.at<Vec3b>(r, c) = P; 
+		}
+	}
+
+	imshow("MASKED IMAGE", image_masked); 
+
+
+	Mat1b mask1 = Mat::zeros(image.size(), CV_8UC1);
+	Mat1b mask2 = Mat::zeros(image.size(), CV_8UC1);
+
+	inRange(image_HSV, Scalar(0, 50, 100), Scalar(6, 200, 255), mask1); // mask 
+	inRange(image_HSV, Scalar(174, 50, 100), Scalar(180, 200, 255), mask2); // mask2 
+
+	Mat1b mask = mask1 | mask2;
+
+	imshow("COLOR MASK", mask);
+
+	mask = mask & image_gray; 
+
+	imshow("COMBINED MASK", mask); 
 	vector<vector<Point>> contours; 
 
 	//findContours(image_gray, contours, RETR_FLOODFILL, CHAIN_APPROX_NONE); 
